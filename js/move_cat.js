f07f697d50bbfd581f3ba60396fb744b00f1049f
LenaAn <anyusheva@phystech.edu>
2021-04-06 14:26:57 +0300

    add timeout when cat is gone

--- js/move_cat.js
+++ js/move_cat.js
@@ -1,10 +1,22 @@
+function catIsGone() {
+	cat.style.display = "none";
+}
+
 const cat = document.getElementById('myCat');
 
-const handler = () => {
+const changePositionHandler = () => {
 	cat.style.position = "absolute";
 	// TODO: account for cat size when calculating position
 	cat.style.left =  Math.random() * 95 + "%";
 	cat.style.top =  Math.random() * 95 + "%";
 }
 
-cat.addEventListener('click', handler);
+const resetTimerHandler = () => {
+	window.clearTimeout(timeoutHandle);
+	timeoutHandle = window.setTimeout(catIsGone, 1000);
+}
+
+
+timeoutHandle = window.setTimeout(catIsGone, 1000);
+cat.addEventListener('click', changePositionHandler);
+cat.addEventListener('click', resetTimerHandler);
